4b197327d545920dbbc3b9e2dbb85952ad70d529
Kirit Narain <kiritnarain@gmail.com>
2021-05-19 20:57:48 -0700

    chord visualization

--- app/public/scripts/add-element-controller.js
+++ app/public/scripts/add-element-controller.js
@@ -1,35 +1,16 @@
 const Strings = [];
 let Markers = [];
+
 for (let i = 1; i <= 6; i++) {
     Strings.push(document.getElementById('String' + i))
-}
-
-const displayStrings = [];
-for (let j = 0; i<3; j++) {
-    for (let i = 1; j <= 6; i++) {
-        displayStrings.push(document.getElementById('screen' + j + 'string' + i));
-    }
 }
 
 const initialOpacity = Strings[0].getAttribute('material').opacity;
 console.log('initial string opacity: ' + initialOpacity);
 
-const displayStringInitialOpacity = 1.0;
-
-function getMidpointPosition(fret1, fret2,screen) {
-    let s = 'screen' + screen + 'fret' + fret1;
-    let a = document.getElementById(s);
-
-    let fret1Pos, fret2Pos;
-
-    if(screen == null){
-        fret1Pos = document.querySelector('#fret' + fret1).object3D.position;
-        fret2Pos = document.querySelector('#fret' + fret2).object3D.position;
-    }else{
-        fre1Pos = document.getElementById('screen' + screen + 'fret' + fret1).object3D.position;
-        fre2Pos = document.getElementById('screen' + screen + 'fret' + fret2).object3D.position;
-    }
-    
+function getMidpointPosition(fret1, fret2) {
+    let fret1Pos = document.querySelector('#fret' + fret1).object3D.position;
+    let fret2Pos = document.querySelector('#fret' + fret2).object3D.position;
 
     const getMid = function (num1, num2) {
         return (num1 + num2) / 2;
@@ -45,13 +26,9 @@ function getMidpointPosition(fret1, fret2,screen) {
 
 // pass screen for displaying in front of user in display
 // do not pass anything for screen or pass as null if you want to add marker to guitar.
-this.addMarker = function (markArray, screen) {
+this.addMarker = function (markArray) {
     let markerScale = '0.005 0.005 0.005'; // scale the size of the marker by this amount.
-    // if screen is null i.e. we are placing a marker on the display ahead of us
-    // so we want to scale it up.
-    if (screen != null) {
-        markerScale = '0.5 0.5 0.5';
-    }
+
     // clear markers and strings
     Markers.forEach((marker) => marker.parentNode.removeChild(marker));
     Markers = [];
@@ -60,19 +37,12 @@ this.addMarker = function (markArray, screen) {
         opacity: initialOpacity
     }));
 
-    displayStrings.forEach((string) => string.setAttribute('material', {
-        color: 'white',
-        opacity: displayStringInitialOpacity
-    }));
-
     // let scaleLength = 0.6477;
     for (let i = 0; i < markArray.length; i++) {
         let strNum = markArray[i][0];
         let fretNum = markArray[i][1];
         let el = Strings[strNum - 1];
-        if (screen != null) { // we are adding to display ahead of us. 
-            el = document.getElementById('screen' + screen + 'fret' + fretNum);
-        }
+
         if (fretNum === 0) {
             // highlight entire string
             el.setAttribute('material', 'color: blue; opacity: 0.6');
@@ -80,31 +50,19 @@ this.addMarker = function (markArray, screen) {
         }
         let newMark = document.createElement('a-entity');
 
-        const midPoint = getMidpointPosition(fretNum, fretNum - 1,screen);
+        const midPoint = getMidpointPosition(fretNum, fretNum - 1);
 
         newMark.setAttribute('geometry', {
             primitive: 'sphere'
         });
-        //newMark.setAttribute('id', "marker" + i); // setting ID numbers so that we can delete them later.
         newMark.setAttribute('material', 'color: blue');
         newMark.setAttribute('scale', markerScale);
-        newMark.setAttribute('position', {
-            x: midPoint.x,
-            y: 0,
-            z: 0
-        });
-        // newMark.object3D.position = midPoint;
+        newMark.object3D.position.set(midPoint.x, 0, 0);
         el.appendChild(newMark);
         Markers.push(newMark);
     }
 }
 
-this.calcFretDistance = function (fretNum) {
-    let scaleLength = 0.6477;
-    let twoPower = Math.pow(2, fretNum / 12);
-    let d = scaleLength - (scaleLength / twoPower);
-    return d;
-}
 window.onload = function(e){
     let firstPoint = [1, 2]; // string 1 , fret 2
     let secondPoint = [2, 2]; // string 2 , fret 2
@@ -112,16 +70,11 @@ window.onload = function(e){
     let fourthPoint = [3, 10]; // string 3, fret 10
     let fifthPoint = [6, 4]; // string 6, fret 4
     let passedArray = [firstPoint, secondPoint, thirdPoint, fourthPoint, fifthPoint];
-    console.log('setting initial string positions');
     this.addMarker(passedArray, null);
-    this.addMarker(passedArray,0);
-    this.addMarker(passedArray,1);
-    this.addMarker(passedArray,2);
 }
 
 
 
 document.querySelector('a-scene').addEventListener('tab-change', (event) => {
     this.addMarker(event.detail.tab);
-    this.addMarker(event.detail.tab, 0);
 });
--- app/public/scripts/screen-tabs.js
+++ app/public/scripts/screen-tabs.js
@@ -1,102 +1,172 @@
 // Code for fret and string addition
-const scaleLength = 0.6477; // this is 25.5 inches in meters. A-frame uses meters so we write it in meters.
 // ---------------- STRING ADDITION ---------------------------------------
 
-function initScreens(){
-    let allVis = ['first', 'second', 'third'];
-    for (let z = 0; z < 3; z++) {
-        let a = 1;
-        let yPosStr = 0;
-        for (a = 1; a <= 6; a++) {
-            let str = document.createElement('a-entity');
-            str.setAttribute('geometry', {
-                primitive: 'box',
-                width: scaleLength * 4,
-                height: 0.001 * 10,
-                depth: 0.001 * 10
-            });
-            str.setAttribute('position', { x: 0, y: yPosStr, z: 0 });
+const allVis = ['first', 'second', 'third'];
 
-            let strID = 'screen' + z;
-            strID = strID + 'string' + a;
-            str.setAttribute('id', strID);
-            str.setAttribute('material', 'color:red');
-            let c = document.getElementById(allVis[z]);
-            c.appendChild(str);
-            yPosStr += 0.2
-        }
+scene = document.querySelector('a-scene');
+
+scene.addEventListener('show-screen-marker', function (event) {
+    let tab = event.detail.tab;
+    let screen = event.detail.screen;
+    generateScreen(screen, tab);
+});
+
+scene.addEventListener('clear-screens', function (event) {
+    for(let el of allVis){
+        removeAllChildNodes(document.getElementById(el));
     }
-// --------------------- START FRET ADDITION-------------------------------------------
+});
 
+function addScreenFret(stringElm, startPos, distanceFromNut, fretNum, displayNum) {
+    let myX = startPos.x;
+    let myY = startPos.y;
+    let myZ = startPos.z;
 
-    const totalFrets = 21; // just going to make 15 frets for now.
-    const scaleFactor = 1.45; // manual scale factor
-    let elm = document.getElementById('screen0string1');
-    console.log(elm);
+     myX = myX + distanceFromNut;
+
+    let newMark = document.createElement('a-entity');
+    newMark.setAttribute('geometry', {
+        primitive: 'box',
+        width: 0.075 * 10 + 0.3,
+        height: 0.03,
+        depth: 0.01
+    });
+    newMark.setAttribute('rotation', '0 0 90');
+    newMark.setAttribute('material', 'color: #c4c4c4');
+    let fretId = 'screen' + displayNum + 'fret' + fretNum;
+    newMark.setAttribute('id', fretId);
+    newMark.object3D.position.set(myX, myY, myZ);
+    stringElm.appendChild(newMark);
+}
+
+function generateScreen(screen, markArray) {
+    let totalFrets = 0;
+    for(let i=0; i<markArray.length; i++){
+        totalFrets = Math.max(markArray[i][1]+1, totalFrets);
+    }
+
+    const interfretDistance = 0.35;
+
+    const stringWidth = (totalFrets -1) * interfretDistance;
+
+    let parent = document.getElementById(allVis[screen]);
+    removeAllChildNodes(parent);
+
+    let yPosStr = 0;
+    for (let a = 1; a <= 6; a++) {
+        let str = document.createElement('a-entity');
+        str.setAttribute('geometry', {
+            primitive: 'box',
+            width: stringWidth,
+            height: 0.03,
+            depth: 0.01
+        });
+        str.setAttribute('position', {x: 0, y: yPosStr, z: 0});
+
+        let strID = 'screen' + screen;
+        strID = strID + 'string' + a;
+        str.setAttribute('id', strID);
+        str.setAttribute('material', 'color: #e3a002');
+
+        parent.appendChild(str);
+        yPosStr += 0.2
+    }
+
+    let elm = document.getElementById('screen' + screen + 'string1');
     let startPos = elm.object3D.position.clone();
-    startPos.x = startPos.x - (scaleLength * 4 / 2);
+    startPos.x = startPos.x - (stringWidth/2);
     startPos.y += 0.5;
 
-    this.addFret = function (distanceFromNut, fretNum, displayNum) {
-        // console.log('adding fret ' + fretNum + ' at distance: ' + distanceFromNut);
-        let myX = startPos.x;
-        let myY = startPos.y;
-        let myZ = startPos.z;
 
-        myX = myX + (distanceFromNut * scaleFactor);
 
-        //let p = el.getAttribute('position');
-        let newMark = document.createElement('a-entity');
-        newMark.setAttribute('geometry', {
-            primitive: 'box',
-            width: 0.075 * 10 + 0.3,
-            height: 0.001 * 10,
-            depth: 0.001 * 10
-        });
-        newMark.setAttribute('rotation', '0 0 90');
-        newMark.setAttribute('material', 'color:red');
-        let fretId = 'screen' + displayNum + 'fret' + fretNum;
-        newMark.setAttribute('id', fretId);
-        newMark.setAttribute('position', {
-            x: myX,
-            y: myY,
-            z: myZ
-        });
-        elm.appendChild(newMark);
+    addScreenFret(elm, startPos, 0, 0, screen); // creating Nut in the virtual world.
+
+    for (let i = 1; i < totalFrets; i++) {
+        let d = interfretDistance * i;
+        addScreenFret(elm, startPos, d, i, screen);
     }
-    elm = document.getElementById('screen0string1');
-    this.addFret(0, 0, 0); // creating Nut in the virtual world.
-    elm = document.getElementById('screen1string1');
-    this.addFret(0, 0, 1);
-    elm = document.getElementById('screen2string1');
-    this.addFret(0, 0, 1);
-    let i = 1;
 
-    for (let n = 0; n < 3; n++) { // add frets to all displays.
-        let idname = 'screen' + n + 'string1'; // we are adding relative to string1.
-        console.log(idname);
-        elm = document.getElementById(idname);
-        for (i = 1; i < totalFrets; i++) {
-            let twoPower = Math.pow(2, i / 12);
-            let d = (scaleLength * 4) - ((scaleLength * 4) / twoPower);
-            this.addFret(d, i);
-        }
-    }
+    addScreenMarker(screen, markArray);
+
+
 }
 
 
+const displayStringInitialOpacity = 1.0;
+let ScreenMarkers = [[], [], []]; // indexed by screen name
+// todo: think through
+/*const displayStrings = [];
+for (let j = 0; i<3; j++) {
+    for (let i = 1; j <= 6; i++) {
+        displayStrings.push(document.getElementById('screen' + j + 'string' + i));
+    }
+}*/
 
-//---------------------- adding marker code -----------------------------------------
+function getScreenMidpointPosition(fret1, fret2, screen) {
+    let fret1Pos = document.getElementById('screen' + screen + 'fret' + fret1).object3D.position;
+    let fret2Pos = document.getElementById('screen' + screen + 'fret' + fret2).object3D.position;
+
+    const getMid = function (num1, num2) {
+        return (num1 + num2) / 2;
+    }
+
+    return {
+        x: getMid(fret1Pos.x, fret2Pos.x),
+        y: getMid(fret1Pos.y, fret2Pos.y),
+        z: getMid(fret1Pos.z, fret2Pos.z)
+    }
+}
+
+// pass screen for displaying in front of user in display
+// do not pass anything for screen or pass as null if you want to add marker to guitar.
+function addScreenMarker(screen, markArray) {
+    let markerScale = '0.05 0.05 0.05';
+
+    // clear markers and strings
+
+    ScreenMarkers[screen].forEach((marker) => marker.parentNode.removeChild(marker));
+    ScreenMarkers[screen] = [];
+    // todo: implement
+    /*displayStrings.forEach((string) => string.setAttribute('material', {
+    color: 'white',
+    opacity: displayStringInitialOpacity
+}));*/
 
 
+    for (let i = 0; i < markArray.length; i++) {
+        let strNum = markArray[i][0];
+        let fretNum = markArray[i][1];
+        let el = document.getElementById('screen' + screen + 'string' + strNum);
 
 
-let firstPoint = [1, 2]; // string 1 , fret 2
-let secondPoint = [2, 2]; // string 2 , fret 2
-let thirdPoint = [5, 7]; //string 5, fret 7
-let fourthPoint = [3, 10]; // string 3, fret 10
-let fifthPoint = [6, 4]; // string 6, fret 4
-let passedArray = [firstPoint, secondPoint, thirdPoint, fourthPoint, fifthPoint];
-this.addMarker(passedArray,0);
-this.addMarker(passedArray,1);
-this.addMarker(passedArray,2);
+        if (fretNum === 0) {
+            // highlight entire string
+            el.setAttribute('material', 'color: blue; opacity: 0.6');
+            return;
+        }
+
+        let newMark = document.createElement('a-entity');
+
+        const midPoint = getScreenMidpointPosition(fretNum, fretNum - 1, screen);
+
+        newMark.setAttribute('geometry', {
+            primitive: 'sphere'
+        });
+
+        newMark.setAttribute('material', 'color: blue');
+        newMark.setAttribute('scale', markerScale);
+
+
+        newMark.object3D.position.set(midPoint.x, 0, 0);
+        el.appendChild(newMark);
+        console.log(el);
+
+        ScreenMarkers[screen].push(newMark);
+    }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
