2667fa5c692df6bf11110f508d200bd8f3cd138d
Rishabhpatted24 <rishabhpatted24@gmail.com>
2021-05-31 23:12:14 +0530

    fixed song selection

--- app/public/scripts/cursor-listener.js
+++ app/public/scripts/cursor-listener.js
@@ -24,15 +24,12 @@ AFRAME.registerComponent('cursor-listener', {
             let manMode_cl = document.getElementById('btn-1');
             let autoMode_cl = document.getElementById('btn-2');
             if (manMode_cl.getAttribute('material').color == '#005f99') {
-                console.log("here1111");
                 this.manual();
-                console.log("here11");
             } else if (autoMode_cl.getAttribute('material').color == '#005f99') {
 
             }
         } else if (this.id == 'drop-down-btn') {
-            console.log("in drop down");
-            let y_co = 0;
+            let y_co = 0.4;
             for(let i = 0; i < songList.length; i++) {
                 let newSong = document.createElement('a-entity');
                 newSong.setAttribute('geometry', 'primitive:plane; height: auto; width: 1');
@@ -44,22 +41,40 @@ AFRAME.registerComponent('cursor-listener', {
                     color: '#99154e'
                 });
                 newSong.setAttribute('position',{
-                    x: 0,
+                    x: 2,
                     y: y_co - 0.21,
-                    z: 0
-                }
-                );
+                    z: -3
+                });
+                newSong.setAttribute('rotation','0 -30 0');
                 y_co -= 0.21;
-                this.appendChild(newSong);
-              //  newSong.setAttribute(cursor-listener);
+                document.getElementById("hud").appendChild(newSong);
+                newSong.setAttribute('cursor-listener','');
                 newSong.setAttribute('id', songList[i]);
             }
 
         } else {
-            console.log(this.id);
+            this.songSelected = this.id;
+           // let songBtn = document.getElementById(this.id);
+           for (let i = 0; i < songList.length; i++) {
+                let itm = document.getElementById(songList[i]);
+                itm.setAttribute('material', 'color: #ffc93c; opacity: 0.6');
+                itm.setAttribute('text', {
+                    width: 3, 
+                    value: songList[i],
+                    align:'center',
+                    color: '#99154e'
+                });
+            }
+            this.setAttribute('material', 'color: #005f99; opacity: 1');
+            this.setAttribute('text', {
+                width:3,
+                value: this.id,
+                align:'center',
+                color: '#fff5b7'
+            });
+
+           
         }
-        
-        console.log("here");
       });
     },
   });
